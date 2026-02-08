298291ba17ab856d4ed13ed129553409a2714de0
Chengzong-Rin <tsheejzoovaj2002@gmail.com>
2026-02-08 13:38:39 +0900

    add some files

--- Main.java
+++ Main.java
@@ -5,7 +5,6 @@ public class Main {
 		UC11and12 uc = new UC11and12();
 		// uc.viewTeacherSch();
 		uc.cancellationProcess();
-		BruteForce bf = new BruteForce();
 		//bf.bruteForceAttack(); //bruteforce attack
 		
 	}
