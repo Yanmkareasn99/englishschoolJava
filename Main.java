4b1ce53b93f1d68bffb43623bb164b84768899af
25j009xayaovong <25j009_xayaovong@edu.kimura.ac.jp>
2026-02-08 13:55:03 +0900

    fixed

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		UC11and12 uc = new UC11and12();
-		// uc.viewTeacherSch();
+		uc.viewTeacherSch();
 		uc.cancellationProcess();
 	}
 }
